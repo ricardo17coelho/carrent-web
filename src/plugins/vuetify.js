a9f908cf9ff6562aeb0682c8379a054e3c9c67c3
Ricardo Coelho <devel@rmorgado.ch>
2021-07-05 21:51:34 +0200

    plugin: add vuetify-toast-snackbar-ng

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,37 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vuetify, { VSnackbar, VBtn, VIcon } from 'vuetify/lib';
+import VuetifyToast from 'vuetify-toast-snackbar-ng';
 
-Vue.use(Vuetify);
-
-export default new Vuetify({
+Vue.use(Vuetify, {
+    components: {
+        VSnackbar,
+        VBtn,
+        VIcon
+    }
 });
+
+Vue.use(VuetifyToast);
+
+const opts = {};
+
+Vue.use(VuetifyToast, {
+    x: 'center',
+    y: 'top',
+    color: 'mdi-information', // default
+    icon: 'mdi-information',
+    iconColor: '', // default
+    classes: ['body-2'],
+    timeout: 3000, // default
+    dismissable: true, // default
+    multiLine: false, // default
+    vertical: false, // default
+    queueable: false, // default
+    showClose: true, // default
+    closeText: '', // default
+    closeIcon: 'mdi-close', // default
+    closeColor: '', // default
+    slot: [], //default
+    property: '$toast' // default
+});
+
+export default new Vuetify(opts);
